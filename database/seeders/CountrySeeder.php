b300c4dbf3ad973a30b5e442a826b6b9ce40243d
Alexander Rudenko <alexander.e.rudenko@gmail.com>
2020-10-07 20:05:11 +0300

    Added Country model, seedeer and factory.

---
+++ database/seeders/CountrySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Country;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CountrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Country::factory()->create(['name' => 'Канада']);
+        Country::factory(9)->create();
+    }
+}
